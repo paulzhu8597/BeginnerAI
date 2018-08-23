645c2ba58b0eea36bac91f8b31b424ac95a652d0
JJZHK <jeffcobile@gmail.com>
2018-08-23 15:36:06 +0800

    公共库

---
+++ lib/utils/utils_style.py
@@ -0,0 +1,41 @@
+from matplotlib import pyplot as plt
+plt.switch_backend('agg')
+import os
+import numpy as np
+
+def plot_test_result(input, target, gen_image, epoch, training=True, save=False, save_dir='output/', show=False, fig_size=(5, 5)):
+    if not training:
+        fig_size = (input.size(2) * 3 / 100, input.size(3)/100)
+
+    fig, axes = plt.subplots(1, 3, figsize=fig_size)
+    imgs = [input, gen_image, target]
+    for ax, img in zip(axes.flatten(), imgs):
+        ax.axis('off')
+        ax.set_adjustable('box-forced')
+        # Scale to 0-255
+        img = (((img[0] - img[0].min()) * 255) / (img[0].max() - img[0].min())).numpy().transpose(1, 2, 0).astype(np.uint8)
+        ax.imshow(img, cmap=None, aspect='equal')
+    plt.subplots_adjust(wspace=0, hspace=0)
+
+    if training:
+        title = 'Epoch {0}'.format(epoch + 1)
+        fig.text(0.5, 0.04, title, ha='center')
+
+    # save figure
+    if save:
+        if not os.path.exists(save_dir):
+            os.mkdir(save_dir)
+        if training:
+            save_fn = save_dir + 'Result_epoch_{:d}'.format(epoch+1) + '.png'
+        else:
+            save_fn = save_dir + 'Test_result_{:d}'.format(epoch+1) + '.png'
+            fig.subplots_adjust(bottom=0)
+            fig.subplots_adjust(top=1)
+            fig.subplots_adjust(right=1)
+            fig.subplots_adjust(left=0)
+        plt.savefig(save_fn)
+
+    if show:
+        plt.show()
+    else:
+        plt.close()
